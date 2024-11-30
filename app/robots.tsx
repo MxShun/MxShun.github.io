5878a5bee692e0e90f8eac3aefac1f4a4d76baf9
MxShun <16466322+MxShun@users.noreply.github.com>
2024-11-30 12:09:42 +0900

    fix: dynamic force static

--- app/robots.tsx
+++ app/robots.tsx
@@ -2,6 +2,8 @@ import type { MetadataRoute } from "next"
 
 const siteUrl = "https://mxshun.github.io";
 
+export const dynamic = "force-static";
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
