5acfdaaba9e5fafc3011d0091b59573154da6b15
MxShun <16466322+MxShun@users.noreply.github.com>
2026-03-25 16:23:56 +0900

    add(certs): Protection of Individual Information Person

--- app/certs/page.tsx
+++ app/certs/page.tsx
@@ -139,6 +139,23 @@ export default function Certs() {
               }
             />
             <Element
+              title="個人情報保護士"
+              description="2026年3月取得"
+              thumbnail={
+                <div className="w-36 h-36 inline-flex items-center justify-center rounded-full bg-solarized-dark dark:bg-solarized-light text-solarized-dark dark:text-solarized-light mb-5">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="56"
+                    height="56"
+                    fill="#000000"
+                    viewBox="0 0 256 256"
+                  >
+                    <path d="M208,24H72A32,32,0,0,0,40,56V224a8,8,0,0,0,8,8H192a8,8,0,0,0,0-16H56a16,16,0,0,1,16-16H208a8,8,0,0,0,8-8V32A8,8,0,0,0,208,24Zm-8,160H72a31.82,31.82,0,0,0-16,4.29V56A16,16,0,0,1,72,40H200Z"></path>
+                  </svg>
+                </div>
+              }
+            />
+            <Element
               title="STARTS Open Water Diver"
               description="2022年9月取得"
               thumbnail={
